a8624f283f8e2d0f30a42875211135099b5e82a7
AlexL70 <alexander.levinson.70@gmail.com>
2021-11-16 22:53:40 +0200

    41. Using an interface

--- main.go
+++ main.go
@@ -7,7 +7,16 @@ type person struct {
 }
 
 func (p person) speak() {
-	fmt.Println("Hello, ", p.first)
+	fmt.Println("Hello,", p.first)
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println("I am a secret agent. My name is", sa.first)
 }
 
 type human interface {
@@ -16,7 +25,12 @@ type human interface {
 
 func main() {
 	p1 := person{
-		first: "James",
+		first: "Miss Moneypenny",
+	}
+
+	sa1 := secretAgent{
+		person: person{first: "James"},
+		ltk:    true,
 	}
 
 	fmt.Printf("%T\n", p1)
@@ -24,5 +38,10 @@ func main() {
 	//	p1 is of type "person", but it is also of type "human" (concrete)
 	//	because whatever has "speak()" method is of type "human" (abstract)
 	var x human = p1
+	x.speak()
 	fmt.Printf("%T\n", x)
+	x = sa1
+	x.speak()
+	fmt.Printf("%T\n", x)
+
 }
