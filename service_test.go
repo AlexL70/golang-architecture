8f97d7b8fbc06983a87d2c37344a929d4d333d4b
AlexL70 <alexander.levinson.70@gmail.com>
2021-11-19 16:13:47 +0200

    51. Testing with gomock

--- service_test.go
+++ service_test.go
@@ -1,37 +1,30 @@
 package architecture
 
 import (
-	"fmt"
+	// "fmt"
 	"testing"
+
+	"github.com/golang/mock/gomock"
 )
 
 type Db map[int]Person
 
-func (m Db) Save(key int, p Person) {
-	m[key] = p
-}
-
-func (m Db) Retrieve(key int) Person {
-	return m[key]
-}
-
-func TestPutWithMongo(t *testing.T) {
-	db := Db{}
-	serv := NewPersonService(db)
+func TestPut(t *testing.T) {
+	ctl := gomock.NewController(t)
+	acc := NewMockAccessor(ctl)
+	serv := NewPersonService(acc)
 	p := Person{First: "Alex"}
+	acc.EXPECT().Save(2, p).MinTimes(1).MaxTimes(1)
 	serv.Put(2, p)
-	got := db.Retrieve(2)
-	if got != p {
-		t.Fatalf("Want %v, got %v", p, got)
-	}
+	ctl.Finish()
 }
 
-func ExamplePut() {
-	db := Db{}
-	serv := NewPersonService(db)
-	p := Person{First: "Alex"}
-	serv.Put(2, p)
-	got := db.Retrieve(2)
-	fmt.Println(got)
-	// Output: {Alex}
-}
+// func ExamplePut() {
+// 	db := Db{}
+// 	serv := NewPersonService(db)
+// 	p := Person{First: "Alex"}
+// 	serv.Put(2, p)
+// 	got := db.Retrieve(2)
+// 	fmt.Println(got)
+// 	// Output: {Alex}
+// }
