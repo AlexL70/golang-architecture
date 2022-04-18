1895faad5ffe4216f363c97b0269626ffd6614a2
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-04-18 20:13:21 +0300

    65. Hands-on exercise #4

---
+++ Ninja Levels/level3/4/go.mod
@@ -0,0 +1,3 @@
+module github.com/AlexL70/golang-architecture/tree/main/Ninja_Levels/level3/4
+
+go 1.18
---
+++ Ninja Levels/level3/4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	timeout := 1000
+	if len(os.Args) > 1 {
+		var err error
+		timeout, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			timeout = 1000
+			fmt.Printf("Wrong timeout value passes: %s. Setting to %d\n", os.Args[1], timeout)
+		}
+	}
+	fmt.Printf("Timeout = %d\n", timeout)
+	ctx := context.Background()
+	ctx, cancelF := context.WithTimeout(ctx, time.Duration(timeout)*time.Millisecond)
+	defer cancelF()
+
+	slowOp(ctx)
+}
+
+func slowOp(ctx context.Context) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		select {
+		case <-ctx.Done():
+			fmt.Println("Slow operation has been cancelled.")
+			return
+		default:
+			fmt.Printf("Slow operation is continued... i = %d\n", i)
+		}
+	}
+	fmt.Println("Hello from slow operation!")
+}
