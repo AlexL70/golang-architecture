84769328fee7c22ca296de31bc9c724a44fef775
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-04-19 18:29:32 +0300

    73. Wrapping errors

--- main.go
+++ main.go
@@ -3,32 +3,53 @@ package main
 import (
 	"errors"
 	"fmt"
-	"time"
 )
 
-type ErrFileNotFound struct {
+type ErrFile struct {
 	FileName string
-	When     time.Time
+	Base     error
 }
 
-func (e ErrFileNotFound) Error() string {
-	return fmt.Sprintf("File %s not found at %v.", e.FileName, e.When)
+func (e ErrFile) Error() string {
+	return fmt.Sprintf("File %s: %v.", e.FileName, e.Base)
 }
 
-//  In this implementation "Is" compares error type instead of string error returns
-func (e ErrFileNotFound) Is(other error) bool {
-	_, ok := other.(ErrFileNotFound)
-	return ok
+func (e ErrFile) Unwrap() error {
+	return e.Base
+}
+
+var ErrNotExist = errors.New("File does not exist")
+
+func OpenFile(fileName string) (string, error) {
+	return "", ErrNotExist
+}
+
+func OpenFile2(fileName string) (string, error) {
+	return "", ErrFile{
+		FileName: fileName,
+		Base:     ErrNotExist,
+	}
 }
 
 func main() {
-	err := ErrFileNotFound{
-		FileName: "mytext.txt",
-		When:     time.Now(),
+	//	Easy way. Wrapping error using Errorf function with %w format option
+	_, err := OpenFile("mytextfile.txt")
+	if err != nil {
+		wrappedError := fmt.Errorf("Unable to open %v: %w", "mytextfile.txt", err)
+		//	fmt.Errors keeps old error so comparison shows it is the same error as it was before wrapping
+		if errors.Is(wrappedError, ErrNotExist) {
+			fmt.Printf("This is still \"%e\" after wrapping.\n", ErrNotExist)
+		}
+		fmt.Println(wrappedError)
 	}
 
-	fmt.Println(err)
-
-	isTheSameError := errors.Is(err, ErrFileNotFound{})
-	fmt.Println(isTheSameError)
+	//	More complicated way. Wrapping error into custom error type that implements Unwrap function
+	//	so that Is function could still compare with original error
+	_, err = OpenFile2("MySecondTextFile.txt")
+	if err != nil {
+		if errors.Is(err, ErrNotExist) {
+			fmt.Printf("This is still \"%e\" after wrapping.\n", ErrNotExist)
+		}
+		fmt.Println(err)
+	}
 }
