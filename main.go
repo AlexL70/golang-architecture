4284bf26a7561d7d3487d689ea0cba308bb221f3
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-04-19 15:11:17 +0300

    71. Creating an error using the standard library

--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
-	"runtime"
-	"time"
 )
 
-func main() {
-	fmt.Println(runtime.NumGoroutine())
-	ctx := context.Background()
-	ctx, cancelF := context.WithCancel(ctx)
+var ErrNotExist = fmt.Errorf("File does not exist: %s", "myfile.txt")
+var ErrUserNotExist = errors.New("User does not exist")
 
-	for i := 0; i < 100; i++ {
-		go func(n int) {
-			fmt.Println("running", n)
-			for {
-				select {
-				case <-ctx.Done():
-					fmt.Println("close", n)
-					return
-				default:
-					fmt.Println("still working", n)
-					time.Sleep(100 * time.Millisecond)
-				}
-				fmt.Println("GOROUTINS RUNNING:", runtime.NumGoroutine())
-			}
-		}(i)
+func main() {
+	err := ErrUserNotExist
+
+	if err == ErrUserNotExist {
+		fmt.Println("You need to register first!")
+	} else {
+		fmt.Println("Unknown error")
 	}
-	time.Sleep(3 * time.Second)
-	cancelF()
-	fmt.Println("Sleeping for 1 seconds")
-	time.Sleep(time.Second)
-	fmt.Println("GOROUTINS RUNNING:", runtime.NumGoroutine())
 }
