86145334a5ba471510a8b74c5eceb96bd4fd3dac
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-04-20 16:45:40 +0300

    78. Why assert errors

--- main.go
+++ main.go
@@ -10,16 +10,17 @@ import (
 func main() {
 	const fName = "rumi.txt"
 	f, err := os.Open(fName)
-	if err != nil {
+	var perr *os.PathError
+	if errors.As(err, &perr) {
 		switch {
-		case errors.Is(err, os.ErrNotExist):
+		case errors.Is(perr, os.ErrNotExist):
 			err = fmt.Errorf("File \"%s\" does not exists: %w", fName, err)
-		case errors.Is(err, os.ErrPermission):
+		case errors.Is(perr, os.ErrPermission):
 			err = fmt.Errorf("You do not have a permission to open \"%s\" file: %w", fName, err)
 		default:
 			err = fmt.Errorf("File \"%s\" cannot be opened: %w", fName, err)
 		}
-		log.Println(err)
+		log.Printf("%s\nOperation: %s\nPath: %s\nIs a timeout: %t\n", err, perr.Op, perr.Path, perr.Timeout())
 		return
 	}
 	defer f.Close()
