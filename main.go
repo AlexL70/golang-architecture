70fd53a4c7df278d046d6273cb998f609686c534
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-04-25 22:43:44 +0300

    94. Equality on structs

--- main.go
+++ main.go
@@ -12,5 +12,16 @@ func (p Person) String() string {
 }
 
 func main() {
-	fmt.Println(Person{FirstName: "Alex", LastName: "Levinson"})
+	p1 := Person{FirstName: "Alex", LastName: "Levinson"}
+	p2 := Person{FirstName: "Tom", LastName: "Waits"}
+	m := map[Person]string{}
+	m[p1] = "seems to be an ordinary person"
+	m[p2] = "is a celebrity"
+	fmt.Println(p1, m[p1])
+	fmt.Println(p2, m[p2])
+	fmt.Println(p1 == p2)
+	p3 := Person{}
+	p3.FirstName = p1.FirstName
+	p3.LastName = p1.LastName
+	fmt.Println(p1 == p3) // do not do it with structures containing pointers; pointers might differ but still refer to the same value
 }
