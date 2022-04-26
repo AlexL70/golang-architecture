8977bbe386c518e7dd67452cfcd25bcfe68bbcee
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-04-26 12:38:05 +0300

    97. Coding stack, heap, escape analysis

--- main.go
+++ main.go
@@ -1,27 +1,24 @@
 package main
 
-import "fmt"
+//Use next command line to run this code
+//go run -gcflags "-m" main.go
+//it would show that y variable is moved to head (instead of stack)
+//Do not use "noinline" directive in a real code unless you have a very
+//good reason to do so.
 
-type Person struct {
-	FirstName string
-	LastName  string
+//go:noinline
+func foo() int {
+	x := 42
+	return x
 }
 
-func (p Person) String() string {
-	return fmt.Sprintf("%s %s", p.FirstName, p.LastName)
+//go:noinline
+func bar() *int {
+	y := 43
+	return &y
 }
 
 func main() {
-	p1 := Person{FirstName: "Alex", LastName: "Levinson"}
-	p2 := Person{FirstName: "Tom", LastName: "Waits"}
-	m := map[Person]string{}
-	m[p1] = "seems to be an ordinary person"
-	m[p2] = "is a celebrity"
-	fmt.Println(p1, m[p1])
-	fmt.Println(p2, m[p2])
-	fmt.Println(p1 == p2)
-	p3 := Person{}
-	p3.FirstName = p1.FirstName
-	p3.LastName = p1.LastName
-	fmt.Println(p1 == p3) // do not do it with structures containing pointers; pointers might differ but still refer to the same value
+	_ = foo()
+	_ = bar()
 }
